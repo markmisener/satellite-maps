4cb7d0bef228901352471dd928daf337815d7b12
Mark Misener <markmisener@gmail.com>
2019-02-20 09:47:57 -0800

    initial commit

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask, request, render_template
+import json
+import settings
+
+
+# configure flask app
+app = Flask(__name__)
+app.config.from_envvar('APP_CONFIG_FILE', silent=True)
+
+# get access token from config file
+MAPBOX_ACCESS_TOKEN = app.config['MAPBOX_ACCESS_TOKEN']
+
+# display at homepage path
+@app.route('/')
+def display():
+    return render_template('index.html', MAPBOX_ACCESS_TOKEN=MAPBOX_ACCESS_TOKEN)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=4000)
