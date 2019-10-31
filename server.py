3939b1819f8d59098df25e7746b56b951d899ad3
Mark Misener <markmisener@gmail.com>
2019-10-31 09:45:36 -0700

    add marker if passed coords

--- server.py
+++ server.py
@@ -12,7 +12,9 @@ MAPBOX_ACCESS_TOKEN = os.environ.get('MAPBOX_ACCESS_TOKEN', None)
 # display at homepage path
 @app.route('/')
 def display():
-    return render_template('index.html', MAPBOX_ACCESS_TOKEN=MAPBOX_ACCESS_TOKEN)
+    lat = request.args.get('lat', 'null')
+    lon = request.args.get('lon', 'null')
+    return render_template('index.html', MAPBOX_ACCESS_TOKEN=MAPBOX_ACCESS_TOKEN, lat=lat, lon=lon)
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
